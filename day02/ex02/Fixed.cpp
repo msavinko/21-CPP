f9a5c9b9eaf74a4039ca987743dd01ae4be14435
marlean <marlean@student.21-school.ru>
2022-09-09 20:02:55 +0300

    upd day02

--- day02/ex02/Fixed.cpp
+++ day02/ex02/Fixed.cpp
@@ -2,18 +2,13 @@
 
 Fixed::Fixed( void ){
 	this->_fpnValue = 0;
-	std::cout << "Default constructor called\n";
 }
 Fixed::~Fixed( void ){
-	std::cout << "Destructor called\n";
 }
 Fixed::Fixed(const Fixed & fixed){
-	std::cout << "Copy constructor called\n";
-	// this->_fpnValue = fixed.getRawBits();
 	*this = fixed;
 }
 Fixed & Fixed::operator=(const Fixed & fixed){
-	std::cout << "Copy assignment operator called\n";
 	if (this == &fixed)
 		return *this;
 	this->_fpnValue = fixed.getRawBits();
@@ -28,11 +23,9 @@ void Fixed::setRawBits(int const raw){
 
 Fixed::Fixed(const int num){
 	this->_fpnValue = num * (1 << this->_statBit);
-	std::cout << "Int constructor called\n";
 }
 Fixed::Fixed(const float num){
 	this->_fpnValue = roundf(num * (1 << this->_statBit)); //округляем флот к инту. 2,5 = 3
-	std::cout << "Float constructor called\n";
 }
 float Fixed::toFloat( void ) const{
 	return ((float)this->_fpnValue / (float)(1 << this->_statBit));
@@ -45,3 +38,4 @@ std::ostream & operator<<(std::ostream & o, Fixed const & fixed){
 	o << fixed.toFloat();
 	return o;
 }
+
